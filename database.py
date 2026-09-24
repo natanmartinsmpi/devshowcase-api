847ef68fa6fb6f2f70f1a7b5b9fbcbfcd368ef82
Natanael <natanempreendedor@gmail.com>
2026-09-24 09:51:49 -0300

    Forcar porta 6543 no pooler IPv4

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 # Nome do arquivo do nosso banco de dados SQLite
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:JA25piNS38sm50@db.joioddbsyjjdglojxduc.supabase.co:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres.joioddbsyjjdglojxduc:JA25piNS38sm50@aws-0-sa-east-1.pooler.supabase.com:6543/postgres"
 
 # O "motor" que vai executar os comandos no banco
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
